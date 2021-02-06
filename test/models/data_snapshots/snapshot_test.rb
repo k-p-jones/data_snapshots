8265b2cc7f275dcfe9a1cfb9d447e269e821c7ad
k-p-jones <kenjones620@yahoo.co.uk>
2021-02-06 17:28:49 +0000

    Remove fixtures

--- test/models/data_snapshots/snapshot_test.rb
+++ test/models/data_snapshots/snapshot_test.rb
@@ -2,17 +2,17 @@ require 'test_helper'
 
 class SnapshotTest < ActiveSupport::TestCase
   test 'it is valid with a name and data' do
-    snap = data_snapshots_snapshots(:valid)
+    snap = DataSnapshots::Snapshot.new(name: 'users', data: { name: 'Bob' })
     assert snap.valid?
   end
 
   test 'it is invalid without a name' do
-    snap = data_snapshots_snapshots(:no_name)
+    snap = DataSnapshots::Snapshot.new(data: { name: 'Bob' })
     refute snap.valid?
   end
 
   test 'it is invalid without data' do
-    snap = data_snapshots_snapshots(:no_data)
+    snap = DataSnapshots::Snapshot.new(name: 'users')
     refute snap.valid?
   end
 end
