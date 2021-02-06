97c4e68b57c79744ce4b10e676a5015da2ee5341
k-p-jones <kenjones620@yahoo.co.uk>
2021-02-06 12:28:10 +0000

    fix namespacing for tests

--- lib/data_snapshots.rb
+++ lib/data_snapshots.rb
@@ -6,6 +6,10 @@ module DataSnapshots
     attr_accessor :configuration
   end
 
+  def self.table_name_prefix
+    'data_snapshots_'
+  end
+
   def self.configure
     self.configuration ||= Configuration.new
     yield(configuration)
--- test/models/data_snapshots/snapshot_test.rb
+++ test/models/data_snapshots/snapshot_test.rb
@@ -2,17 +2,17 @@ require 'test_helper'
 
 class SnapshotTest < ActiveSupport::TestCase
   test 'it is valid with a name and data' do
-    snap = snapshots(:valid)
+    snap = data_snapshots_snapshots(:valid)
     assert snap.valid?
   end
 
   test 'it is invalid without a name' do
-    snap = snapshots(:no_name)
+    snap = data_snapshots_snapshots(:no_name)
     refute snap.valid?
   end
 
   test 'it is invalid without data' do
-    snap = snapshots(:no_data)
+    snap = data_snapshots_snapshots(:no_data)
     refute snap.valid?
   end
 end
