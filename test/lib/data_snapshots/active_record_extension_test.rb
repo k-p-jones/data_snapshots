3e508cca432f0d9bf7211bb21001429b1fb0e4db
k-p-jones <kenjones620@yahoo.co.uk>
2021-02-13 15:42:16 +0000

    Improve test coverage

--- test/lib/data_snapshots/active_record_extension_test.rb
+++ test/lib/data_snapshots/active_record_extension_test.rb
@@ -17,19 +17,27 @@ class ActiveRecordExtensionTest < ActiveSupport::TestCase
     end
 
     DataSnapshots.generate_snapshots(name: :users_age, collection: User.all)
+    User.last.update(age: 31)
+    DataSnapshots.generate_snapshots(name: :users_age, collection: User.all)
+
+    DataSnapshots.generate_snapshots(name: :users_name, collection: User.all)
+    User.last.update(name: 'Steve')
     DataSnapshots.generate_snapshots(name: :users_name, collection: User.all)
   end
 
   test '#<name>_snapshots' do
     user = User.last
-    assert_equal 1, user.users_age_snapshots.count
-    assert_equal 1, user.users_name_snapshots.count
+    assert_equal 2, user.users_age_snapshots.count
+    assert_equal 2, user.users_name_snapshots.count
 
-    snap_1 = user.users_age_snapshots.first
-    snap_2 = user.users_name_snapshots.first
+    age_snaps = user.users_age_snapshots
+    name_snaps = user.users_name_snapshots
 
-    assert_equal 23, snap_1.data['current_age']
-    assert_equal 'Bob', snap_2.data['current_name'] 
+    assert_equal 23, age_snaps[0].data['current_age']
+    assert_equal 31, age_snaps[1].data['current_age']
+
+    assert_equal 'Bob', name_snaps[0].data['current_name']
+    assert_equal 'Steve', name_snaps[1].data['current_name']
   end
 
   test '#generate_snapshot' do
@@ -39,7 +47,7 @@ class ActiveRecordExtensionTest < ActiveSupport::TestCase
       user.generate_snapshot(name: :users_name)
     end
 
-    assert_equal 2, user.users_name_snapshots.count
+    assert_equal 3, user.users_name_snapshots.count
 
     # passing the name of a snapshot that hasn't been registered
     assert_raises UnregisteredSnapshotError do
